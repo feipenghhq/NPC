0766a9f22ab32079e089e1d0be384bfe4a07dcfa
Heqing Huang <feipenghhq@gmail.com>
2024-06-27 21:42:15 -0700

    Create AXI4Lite arbiter and decoder

---
+++ core/src/spinal/bus/Axi4Lite/Axi4LiteArbiter.scala
@@ -0,0 +1,99 @@
+/* ------------------------------------------------------------------------------------------------
+ * Copyright (c) 2023. Heqing Huang (feipenghhq@gmail.com)
+ *
+ * Project: NRC
+ * Author: Heqing Huang
+ * Date Created: 6/24/2024
+ *
+ * ------------------------------------------------------------------------------------------------
+ * Axi4Lite Arbiter
+ * ------------------------------------------------------------------------------------------------
+ */
+package bus.Axi4Lite
+
+import spinal.core._
+import spinal.lib._
+import config._
+
+case class Axi4LiteArbiter(config: Axi4LiteConfig, count: Int) extends Component {
+    val io = new Bundle {
+        val input = Vec(slave(Axi4Lite(config)), count)
+        val output = master(Axi4Lite(config))
+    }
+    noIoPrefix()
+
+    // Use a round robin arbiter for now, the arbitration result can change
+    // if the downstream port is not ready
+    def _arbiter[T <: Data](inputs: IndexedSeq[Stream[T]], consumed: Bool) = new Area {
+        val size = inputs.size
+        val grantOH = UInt(size bits)
+
+        val lastGrant = RegNextWhen(grantOH, consumed) init 1 // record that previous grantOH for the
+        val req = inputs.map(_.valid).asBits().asUInt
+        val doubleReq = req @@ req
+        val doubleGrant = doubleReq & (doubleReq - lastGrant.rotateLeft(1).resized)
+        grantOH := doubleGrant(0, size bits) | doubleGrant(size, size bits)
+        val grant = OHToUInt(grantOH)
+    }
+
+    val read = new Area {
+        val ar = io.input.map((f: Axi4Lite) => f.ar)
+        val r = io.input.map((f: Axi4Lite) => f.r)
+
+        val consumed = io.output.ar.fire
+        val arbiter = _arbiter(ar, consumed)
+
+        // store the wining result for route the transaction back
+        // the AXI4Lite does not support outstanding transaction so read response
+        // must come back before a new request
+        val selectedOH = RegNextWhen(arbiter.grantOH, io.output.ar.fire) init 1
+        val respSelOH = io.output.ar.fire ? arbiter.grantOH | selectedOH
+
+        // AR channel
+        io.output.ar.valid := ar.map(_.valid).reduceBalancedTree(_ || _)
+        io.output.ar.payload := ar.map(_.payload).read(arbiter.grant)
+        ar.zipWithIndex.foreach(f => f._1.ready := io.output.ar.ready & arbiter.grantOH(f._2))
+
+        // R channel
+        io.output.r.ready := (r.map(_.ready).asBits & respSelOH.asBits).orR
+        r.zipWithIndex.foreach(f => f._1.valid := io.output.r.valid & respSelOH(f._2))
+        r.foreach(_.payload := io.output.r.payload)
+    }
+
+    val write = new Area {
+        val aw = io.input.map((f: Axi4Lite) => f.aw)
+        val w = io.input.map((f: Axi4Lite) => f.w)
+        val b = io.input.map((f: Axi4Lite) => f.b)
+
+        val consumed = io.output.aw.fire
+        val arbiter = _arbiter(aw, consumed)
+
+        // store the wining result for route the transaction back
+        // the AXI4Lite does not support outstanding transaction so read response
+        // must come back before a new request
+        val selectedOH = RegNextWhen(arbiter.grantOH, io.output.aw.fire) init 1
+        val channelSelOH = io.output.aw.fire ? arbiter.grantOH | selectedOH
+        val channelSel = OHToUInt(channelSelOH)
+
+        // AW channel
+        io.output.aw.valid := aw.map(_.valid).reduceBalancedTree(_ || _)
+        io.output.aw.payload := aw.map(_.payload).read(arbiter.grant)
+        aw.zipWithIndex.foreach(f => f._1.ready := io.output.aw.ready & arbiter.grantOH(f._2))
+
+        // W channel
+        io.output.w.valid := w.map(_.valid).reduceBalancedTree(_ || _)
+        io.output.w.payload := w.map(_.payload).read(channelSel)
+        w.zipWithIndex.foreach(f => f._1.ready := io.output.aw.ready & channelSelOH(f._2))
+
+        // B channel
+        io.output.b.ready := (b.map(_.ready).asBits & channelSelOH.asBits).orR
+        b.zipWithIndex.foreach(f => f._1.valid := io.output.r.valid & channelSelOH(f._2))
+        b.foreach(_.payload := io.output.b.payload)
+    }
+
+}
+
+object Axi4LiteArbiterVerilog extends App {
+    val config = Axi4LiteConfig()
+    Config.spinal.generateVerilog(Axi4LiteArbiter(config, 4))
+}
---
+++ core/src/spinal/bus/Axi4Lite/Axi4LiteDecoder.scala
@@ -0,0 +1,79 @@
+/* ------------------------------------------------------------------------------------------------
+ * Copyright (c) 2023. Heqing Huang (feipenghhq@gmail.com)
+ *
+ * Project: NRC
+ * Author: Heqing Huang
+ * Date Created: 6/23/2024
+ *
+ * ------------------------------------------------------------------------------------------------
+ * Axi4Lite Decoder
+ * ------------------------------------------------------------------------------------------------
+ */
+package bus.Axi4Lite
+
+import spinal.core._
+import spinal.lib._
+import config._
+
+case class Axi4LiteDecoder(config: Axi4LiteConfig, decoding: Seq[Range]) extends Component {
+    val io = new Bundle {
+        val input = slave(Axi4Lite(config))
+        val output = Vec(master(Axi4Lite(config)), decoding.size)
+    }
+    noIoPrefix()
+
+    val read = new Area {
+        // Decode the AR channel
+        val araddr = io.input.ar.payload.araddr
+        val hits = decoding.map(r => (araddr >= r.start && araddr <= r.end))
+        val hitsBuffer = RegNextWhen(hits.asBits, io.input.ar.valid) init 0
+        for ((output, hit) <- io.output.zip(hits)) {
+            output.ar.payload <> io.input.ar.payload
+            output.ar.valid := io.input.ar.valid & hit
+        }
+        io.input.ar.ready := (io.output.map(_.ar.ready).asBits & hits.asBits).orR
+
+        // Route back the R channel
+        val respHits = io.input.ar.valid ? hits.asBits | hitsBuffer
+        io.output.foreach(ch => ch.r.ready := io.input.r.ready)
+        io.input.r.valid := (io.output.map(_.r.valid).asBits & respHits).orR
+        io.input.r.payload.rdata := MuxOH(respHits, io.output.map(_.r.payload.rdata))
+        io.input.r.payload.rresp := MuxOH(respHits, io.output.map(_.r.payload.rresp))
+    }
+
+    val write = new Area {
+        // Decode the AW channel
+        val awaddr = io.input.aw.payload.awaddr
+        val hits = decoding.map(r => (awaddr >= r.start && awaddr <= r.end))
+        val hitsBuffer = RegNextWhen(hits.asBits, io.input.aw.valid) init 0
+        for ((output, hit) <- io.output.zip(hits)) {
+            output.aw.payload <> io.input.aw.payload
+            output.aw.valid := io.input.aw.valid & hit
+        }
+        io.input.aw.ready := (io.output.map(_.ar.ready).asBits & hits.asBits).orR
+
+        // Route the W channel
+        val hitsFinal = io.input.aw.valid ? hits.asBits | hitsBuffer
+        for ((output, hit) <- io.output.zip(hitsFinal.subdivideIn(1 bits))) {
+            output.w.payload <> io.input.w.payload
+            output.w.valid := io.input.w.valid & hit.asBool
+        }
+        io.input.w.ready := (io.output.map(_.w.ready).asBits & hitsFinal).orR
+
+        // Route back the B channel
+        io.output.foreach(ch => ch.b.ready := io.input.b.ready)
+        io.input.b.valid := (io.output.map(_.b.valid).asBits & hitsFinal).orR
+        io.input.b.payload.bresp := MuxOH(hitsFinal, io.output.map(_.b.payload.bresp))
+    }
+}
+
+object Axi4LiteDecoderVerilog extends App {
+    val config = Axi4LiteConfig()
+    val decoding = Seq(
+        Range(0x0, 0x0FFFFFFF),
+        Range(0x10000000, 0x1FFFFFFF),
+        Range(0x20000000, 0x2FFFFFFF),
+        Range(0x30000000, 0x3FFFFFFF),
+    )
+    Config.spinal.generateVerilog(Axi4LiteDecoder(config, decoding))
+}
